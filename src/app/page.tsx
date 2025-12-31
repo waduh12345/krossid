024a1b6a06b22b1fd1e2645e23dd1a27f76a4da2
sonice007 <soni.setiawan.it07@gmail.com>
2025-12-31 09:45:03 +0700

    fix update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import {  useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { subscribeUser, unsubscribeUser, sendNotification } from "./actions";
-
-// ...fungsi urlBase64ToUint8Array dan komponen lain tetap...
 
 export default function Page() {
   const router = useRouter();
